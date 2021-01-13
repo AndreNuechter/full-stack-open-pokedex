1be04c656466067e77bdb21b282d835b0d59a5c5
AN <andre_nuechter@hotmail.de>
2021-01-13 20:54:12 +0100

    Some change

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static('dist'));
 
-app.get('/health', (_, res) => {
-    res.send('ok');
-});
-
 app.get('/version', (_, res) => {
     res.send('1');
+});
+
+app.get('/health', (_, res) => {
+    res.send('ok');
 });
 
 app.listen(PORT, () => {
